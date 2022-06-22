2eae7019cc6c3ae8817c0678e81d1c7cae61d007
Sergio Basurto <35089326+sebasurto@users.noreply.github.com>
2022-06-22 17:44:02 -0500

    cita

--- TallerUML/src/sistemaClinico/historiaClinica/Cita.java
+++ TallerUML/src/sistemaClinico/historiaClinica/Cita.java
@@ -1,11 +1,28 @@
 package sistemaClinico.historiaClinica;
 import java.util.Date;
 
+import sistemaClinico.usuarios.Doctor;
+import sistemaClinico.usuarios.Paciente;
+
 public class Cita {
 	public Date fecha;
 	public boolean pagada;
 	public String registradoPor;
+	public Paciente paciente;
+	public Doctor doctor;
 	
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	public Doctor getDoctor() {
+		return doctor;
+	}
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
 	public void realizarPago (float pago) {
 		this.pagada= true;
 	}
