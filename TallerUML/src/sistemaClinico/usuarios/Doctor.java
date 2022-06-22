34eab6f7ac9a3fba8e95489afe9197a80f8b0d91
stephcarol <85514342+stephcarol@users.noreply.github.com>
2022-06-22 19:28:21 -0400

    Update Doctor.java

--- TallerUML/src/sistemaClinico/usuarios/Doctor.java
+++ TallerUML/src/sistemaClinico/usuarios/Doctor.java
@@ -3,11 +3,13 @@ package sistemaClinico.usuarios;
 public class Doctor extends Persona {
 	protected int regDoctor;
 	protected String especialidad;
+	protected ArrayList<Cita> cita;
 	
-    public Doctor(int regDoctor, String especialidad) {
+    public Doctor(int regDoctor, String especialidad, ArrayList<Cita> cita) {
 		super();
 		this.regDoctor = regDoctor;
 		this.especialidad = especialidad;
+	    	this.cita = cita;
 	}
 	public int getRegDoctor() {
 		return regDoctor;
@@ -21,9 +23,14 @@ public class Doctor extends Persona {
 	public void setEspecialidad(String especialidad) {
 		this.especialidad = especialidad;
 	}
+	
+	
+	
 	public void recetar(){}
     public void agregarPlanNut(){}
     public void ImprimirReceta(){}
     public void registrarSecretaria(){}
+	
+	public Cita AtenderPaciente(){}
 	
 }
