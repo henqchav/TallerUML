9f7dab04251fb0404a0043584c874fdfefc2537a
Sergio Basurto <35089326+sebasurto@users.noreply.github.com>
2022-06-22 17:37:11 -0500

    cita

--- TallerUML/src/sistemaClinico/historiaClinica/Cita.java
+++ TallerUML/src/sistemaClinico/historiaClinica/Cita.java
@@ -1,5 +1,32 @@
 package sistemaClinico.historiaClinica;
+import java.util.Date;
 
 public class Cita {
-
+	public Date fecha;
+	public boolean pagada;
+	public String registradoPor;
+	
+	public void realizarPago (float pago) {
+		this.pagada= true;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public boolean isPagada() {
+		return pagada;
+	}
+	public void setPagada(boolean pagada) {
+		this.pagada = pagada;
+	}
+	public String getRegistradoPor() {
+		return registradoPor;
+	}
+	public void setRegistradoPor(String registradoPor) {
+		this.registradoPor = registradoPor;
+	}
+	
 }
+
--- TallerUML/src/sistemaClinico/historiaClinica/HistoriaClinica.java
+++ TallerUML/src/sistemaClinico/historiaClinica/HistoriaClinica.java
@@ -1,5 +1,46 @@
 package sistemaClinico.historiaClinica;
 
-public class HistoriaClinica {
+import java.util.ArrayList;
 
+public class HistoriaClinica<E> {
+	public int nro;
+	public ArrayList<E> enfermdades = new ArrayList<E>();
+	public ArrayList<E> alergias = new ArrayList<E>();
+	public ArrayList<E> otros = new ArrayList<E>();
+	
+	public HistoriaClinica(int nro, ArrayList<E> enfermdades, ArrayList<E> alergias, ArrayList<E> otros) {
+		super();
+		this.nro = nro;
+		this.enfermdades = enfermdades;
+		this.alergias = alergias;
+		this.otros = otros;
+	}
+	
+	public int getNro() {
+		return nro;
+	}
+
+	public void setNro(int nro) {
+		this.nro = nro;
+	}
+	public ArrayList<E> getEnfermdades() {
+		return enfermdades;
+	}
+	public void setEnfermdades(ArrayList<E> enfermdades) {
+		this.enfermdades = enfermdades;
+	}
+	public ArrayList<E> getAlergias() {
+		return alergias;
+	}
+	public void setAlergias(ArrayList<E> alergias) {
+		this.alergias = alergias;
+	}
+	public ArrayList<E> getOtros() {
+		return otros;
+	}
+	public void setOtros(ArrayList<E> otros) {
+		this.otros = otros;
+	}
+	
+	
 }
